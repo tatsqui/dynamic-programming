8ed2a04768f434c9a0b24d97cd140926583d4dff
CheezItMan <chris@adadevelopersacademy.org>
2019-09-28 20:49:54 -0700

    Tests for newman conway

---
+++ test/newman_conway_test.rb
@@ -0,0 +1,25 @@
+require_relative "test_helper"
+
+describe "Newman Conway Tests" do
+  it "works with 13" do
+    # Arrange
+    input = 13
+
+    # Act
+    answer = newman_conway(input)
+
+    # Assert
+    expect(answer).must_equal "1 1 2 2 3 4 4 4 5 6 7 7 8"
+  end
+
+  it "works with 20" do
+    # Arrange
+    input = 20
+
+    # Act
+    answer = newman_conway(input)
+
+    # Assert
+    expect(answer).must_equal "1 1 2 2 3 4 4 4 5 6 7 7 8 8 8 8 9 10 11 12"
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,9 @@
+  
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/reporters'
+require "minitest/skip_dsl"
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative '../lib/newman_conway'
