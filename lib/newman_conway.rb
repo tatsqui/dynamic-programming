90e1b0136dbe6b26f7418b0419157656ed953686
CheezItMan <chris@adadevelopersacademy.org>
2019-09-28 21:08:17 -0700

    solution working

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,5 +1,25 @@
 
 
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError, "num cannot be <= 0" if num <= 0
+
+  return "1" if num == 1
+  memo = Array.new(num)
+  memo[0] = 0
+  memo[1] = 1
+  memo[2] = 1
+  output = "1"
+
+  count = 3
+  while count <= num
+    # P(P(n - 1)) + P(n - P(n - 1))
+    memo[count] = memo[memo[count - 1]] + memo[count - memo[count - 1]]
+    count += 1
+  end
+
+  (2..num).each do |i|
+    output += " #{memo[i]}"
+  end
+  
+  return output
 end
