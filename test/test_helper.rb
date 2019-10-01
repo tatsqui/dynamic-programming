6d70ac2c8818b100a1db51cd7b8e4c6094c097b4
CheezItMan <chris@adadevelopersacademy.org>
2019-10-01 14:14:53 -0700

    tests written

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,4 +6,5 @@ require "minitest/skip_dsl"
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-require_relative '../lib/newman_conway'
+require_relative "../lib/newman_conway"
+require_relative "../lib/max_subarray"
